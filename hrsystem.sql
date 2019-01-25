46ce00084224f9da2cf0868c9575e089716116d7
Your Name <you@example.com>
2019-01-25 05:19:49 -0800

    kin photos and db were added

---
+++ hrsystem.sql
@@ -0,0 +1,163 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 25, 2019 at 01:19 PM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hrsystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `companies`
+--
+
+DROP TABLE IF EXISTS `companies`;
+CREATE TABLE IF NOT EXISTS `companies` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `company_phone_numbeer` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `company_size` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `companies`
+--
+
+INSERT INTO `companies` (`id`, `company_name`, `company_phone_numbeer`, `company_size`, `created_at`, `updated_at`) VALUES
+(1, 'Acompany', '11111111111111', '11-20', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_types`
+--
+
+DROP TABLE IF EXISTS `employee_types`;
+CREATE TABLE IF NOT EXISTS `employee_types` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `company_id` int(11) DEFAULT NULL,
+  `employee_type_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `employee_types`
+--
+
+INSERT INTO `employee_types` (`id`, `company_id`, `employee_type_name`, `created_at`, `updated_at`) VALUES
+(1, NULL, 'Full-time', '2019-01-22 13:07:03', '2019-01-22 13:07:03'),
+(2, NULL, 'Part-time', '2019-01-22 13:07:03', '2019-01-22 13:07:03'),
+(3, 1, 'Contractor', '2019-01-22 13:19:04', '2019-01-22 13:19:04'),
+(4, 2, 'Intern', '2019-01-22 13:19:24', '2019-01-22 13:19:24'),
+(5, 1, 'Contractor1', '2019-01-22 13:19:04', '2019-01-22 13:19:04'),
+(6, 1, 'Contractor2', '2019-01-22 13:19:04', '2019-01-22 13:19:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+DROP TABLE IF EXISTS `migrations`;
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(5, '2014_10_12_000000_create_users_table', 3),
+(2, '2014_10_12_100000_create_password_resets_table', 2),
+(3, '2019_01_22_081823_create_companies_table', 2),
+(4, '2019_01_22_155011_employee_types_table', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+DROP TABLE IF EXISTS `password_resets`;
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `work_email` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `start_date` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `employee_type_id` int(11) DEFAULT NULL,
+  `date_of_birth` date DEFAULT NULL,
+  `gender` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `address` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `address_2` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone_number` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `office_phone_number` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `country` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `city` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `state` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `postal_code` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `profile_image` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `biography` mediumtext COLLATE utf8mb4_unicode_ci,
+  `salary` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `terms` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `last_name`, `password`, `email`, `email_verified_at`, `work_email`, `start_date`, `employee_type_id`, `date_of_birth`, `gender`, `address`, `address_2`, `phone_number`, `office_phone_number`, `country`, `city`, `state`, `postal_code`, `profile_image`, `biography`, `salary`, `terms`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Arevaaaaa', 'Arevyan', '$2y$10$1GmUUomtej28Fjhtx/LWw.QEUjU2GRra/XZhQYEDZgThnBkveEKpC', 'arev@gmail.com', NULL, 'arevwork@gmail.com', '2019-01-22', NULL, '2094-05-09', 'male', 'Avan', NULL, '123 456', '123', 'Armenia', 'Yerevan', NULL, '0063', NULL, NULL, NULL, NULL, NULL, '2019-01-23 01:40:14', '2019-01-24 01:20:52');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
